3bf1bfc3470d643dfb002f26f0ce1d5e64551572
Greg Allen <mail@jga23.com>
2011-01-27 20:20:49 -0800

    added phonegap project

---
+++ Recurring/Classes/RecurringAppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  RecurringAppDelegate.h
+//  Recurring
+//
+//  Created by Greg Allen on 1/27/11.
+//  Copyright DemandMedia 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "PhoneGapDelegate.h"
+
+@interface RecurringAppDelegate : PhoneGapDelegate {
+}
+
+@end
+
---
+++ Recurring/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Recurring
+//
+//  Created by Greg Allen on 1/27/11.
+//  Copyright DemandMedia 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"RecurringAppDelegate");
+    [pool release];
+    return retVal;
+}
